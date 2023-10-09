7e39f4f857b23383e4c8c909241531dfdf559b15
luis Colina <colina.1990lz@gmail.com>
2023-10-09 10:41:45 -0400

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,16 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        'comi': ['Commissioner', 'sans-serif'],
+      },
     },
+    colors: {
+      'soft-orange': 'hsl(0, 100%, 79%)',
+      'hard-red' : 'hsl(0, 93%, 64%)',
+      'medium-gray': 'hsl(240, 12%, 30%)',
+      'dark-blue': 'hsl(240, 14%, 11%)',
+    }
   },
   plugins: [],
 }
