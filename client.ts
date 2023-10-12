b24a999c5a043f4f43d2be3cd9275e8cf3b790ee
luis Colina <colina.1990lz@gmail.com>
2023-10-11 20:31:19 -0400

    routing a server side function

--- client.ts
+++ client.ts
@@ -1,8 +1,11 @@
 // client.ts
 import { createClient } from '@sanity/client'
 
-export default createClient({
+const client = createClient({
     projectId: '05qno148',
     dataset: 'production',
+    apiVersion: "2023-10-11",
     useCdn: true // `false` if you want to ensure fresh data
 })
+
+export default client
