c0e94108c9f996b6ff6e06a20ca3318c50f7b183
luis Colina <colina.1990lz@gmail.com>
2023-10-19 08:43:03 -0400

    logo a navbar styling added

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,11 +1,19 @@
+import Image from "next/image"
 import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <nav>
-        <Link href="./">LC</Link>
-        <ul>
-            <li><Link href="./post">all post</Link></li>
+    <nav className="flex items-center justify-between font-semibold ">
+        <Link href="./"><Image 
+              src="/svg/logo-black.svg"
+              width={90}
+              height={90}
+              alt="Picture of the author"
+        /></Link>
+        <ul className="flex gap-5 pr-2">
+            <li><Link href="./post">Post</Link></li>
+            <li><Link href="./post">Contact</Link></li>
+            <li><Link href="./post">About me</Link></li>
         </ul>
     </nav>
   )
