763ac61e7fcf58c22ca8de80fab1d25d78ff0e3d
luis Colina <colina.1990lz@gmail.com>
2023-11-06 10:13:56 -0400

    optimization of hero section

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,19 @@ export default async function Home() {
   const post = await getData();
   console.log(post);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
-      <Hero />
-    </main>
+    <>
+      <section className=" max-w-7xl mx-auto py-24 lg:px-10 md:px-10 px-10">
+        <h1 className="text-3xl font-semibold mb-6 font-comi">
+          Latest Articles
+        </h1>
+        <Hero />
+      </section>
+      <section className=" max-w-7xl mx-auto p-10 lg:px-10 md:px-10 ">
+        <h2 className="text-3xl font-semibold mb-6 font-comi">
+          Web Development
+        </h2>
+      </section>
+    </>
   );
 }
 
--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <div className="bg-black grid min-w-full grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
+    <main className="bg-black grid min-w-full grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
       <div className="bg-soft-orange p-6 shadow-md col-span-1 row-span-3 md:row-span-2 md:col-span-3 lg:col-span-2 lg:row-span-3">
         Main Item
       </div>
@@ -13,7 +13,7 @@ const Hero = () => {
       <div className="bg-soft-orange p-6 shadow-md md:col-start-3 md:row-start-3 lg:row-start-3 lg:col-start-3">
         Item 3
       </div>
-    </div>
+    </main>
   );
 };
 
